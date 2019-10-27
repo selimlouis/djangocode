b9d7ee3c574bdaa59613bb076d9c0cec1689f283
selimlouis <selimseyhanoglu@gmail.com>
2019-10-27 14:42:38 -0400

    added templates and static media

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,1 +1,16 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):
+    context = RequestContext(request)
+
+    context_dict = {'boldmessage': "I am bold from the context"}
+
+
+    return render_to_response('rango/index.html', context_dict, context)
+
+
+def about(request):
+    return HttpResponse("Rango says: Here is the about page")
